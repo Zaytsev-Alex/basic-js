1cd3bccfe5e61530a79bffa4614a31be0dee8256
Zaytsev Alexander <pofi.game@gmail.com>
2020-03-03 20:10:36 +0300

    init: complete Hanoi tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,8 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let result = {};
+    let seconds = turnsSpeed / 3600;
+    let turns = Math.pow(2, disksNumber) - 1;
+    result.turns = turns;
+    result.seconds = turns / seconds;
+    return result;
 }
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date.getMonth() == NaN || date.getMonth() == undefined ){
+    throw new Error();
+  } else if (date == undefined)
+    return 'Unable to determine the time of year!';
+  let month = date.getMonth() + 1;
+  let result;
+  if (month >= 3 && month <= 5) 
+    result = 'spring';
+  else 
+    if (month >= 6 && month <= 8)
+      result = 'summer';
+    else 
+      if (month >= 9 && month <=11)
+        result = 'autumn';
+      else
+        if (month == 12 || month == 1 || month == 2)
+          result = 'winter';
+  return result;
 };
