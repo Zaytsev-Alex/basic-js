12fae22b8d75ff596c3c3aa1c578c36bde79a281
Zaytsev Alexander <pofi.game@gmail.com>
2020-03-07 21:30:07 +0300

    init: complete depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,10 @@
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        let result = 0;
+        for (let i = 0 ; i < arr.length ; i++){
+            if (arr[i] instanceof Array)
+                result = Math.max(this.calculateDepth(arr[i]), result);
+        }
+        return result+1;
     }
 };
